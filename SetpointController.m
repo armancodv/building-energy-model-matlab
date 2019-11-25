c9f1b62905bee17e9c7d198723db9b455be61d8d
akolahan <akolahan@gmail.com>
2019-11-25 15:22:05 +0100

    SetpointController improvement

--- SetpointController.m
+++ SetpointController.m
@@ -3,21 +3,24 @@ classdef SetpointController
         minimum
         maximum
         mode % 1=heating, 2=cooling
+        status
     end
     
     methods
-        function obj = SetpointController(minimum, maximum, mode)
+        function obj = SetpointController(minimum, maximum, mode, status)
             obj.minimum = minimum;
             obj.maximum = maximum;
-            obj.mode = mode;            
+            obj.mode = mode;
+            obj.status = status;
         end
         
         function status = check(obj, temperature)
             if(((obj.mode==1)&&(temperature<=obj.minimum))||((obj.mode==2)&&(temperature>=obj.maximum)))
-                status = 1;
+                obj.status = 1;
             elseif(((obj.mode==1)&&(temperature>=obj.maximum))||((obj.mode==2)&&(temperature<=obj.minimum)))
-                status = 0;
+                obj.status = 0;
             end
+            status = obj.status;
         end
     end
 end
